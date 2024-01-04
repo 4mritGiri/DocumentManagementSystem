eae31e656cb778f33ee2815845088a3e56a7d51d
4mritGiri <amritgiri02595@gmail.com>
2024-01-05 00:08:44 +0545

    Add destruction time mapping and update destruction eligibility

--- DestructionEligible/models.py
+++ DestructionEligible/models.py
@@ -1,52 +1,40 @@
 from django.db import models
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 from Package.models import Package
 from datetime import timedelta
 from django.utils import timezone
 
 class DestructionEligible(models.Model):
+    DESTRUCTION_TIME_MAPPING = {
+        '1 Year': 31536000,
+        '2 Years': 63072000,
+        '3 Years': 94608000,
+        '4 Years': 126144000,
+        '5 Years': 157680000,
+        '6 Years': 189216000,
+        '7 Years': 220752000,
+        '8 Years': 252288000,
+    }
+
     destruction_eligible_id = models.AutoField(primary_key=True)
-    related_package = models.ForeignKey(Package, on_delete=models.CASCADE, related_name='destruction_eligibilities')
+    related_package = models.OneToOneField(Package, on_delete=models.CASCADE, related_name='destruction_eligibility')
     is_expired = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
-    destruction_time = models.IntegerField(null = True, blank = True)
+    destruction_time = models.IntegerField(null=True, blank=True)
 
     def check_is_expired(self):
-        # Check if expiration time has passed
-        expiration_time = self.created_at + timedelta(seconds=self.destruction_time if self.destruction_time else 100)
+        expiration_time = self.created_at + timedelta(seconds=self.destruction_time or 100)
         expired = expiration_time <= timezone.now()
-
-        # Update is_expired based on expiration status
         self.is_expired = expired
-
         return expired
 
     def save(self, *args, **kwargs):
-        # Set created_at to the value of Package's created_at
-        if not hasattr(self, 'id') and self.related_package:  
+        if self.related_package:   
             self.created_at = self.related_package.created_at
 
-        # Set destruction_eligible_time to the value of Package's destruction_eligible_time
-        if hasattr(self.related_package, 'destruction_eligible_date'):
-            destruction_eligible_date = self.related_package.destruction_eligible_time
-
-            if destruction_eligible_date == '1 Year':
-                self.destruction_time = 31536000
-            elif destruction_eligible_date == '2 Years':
-                self.destruction_time = 63072000
-            elif destruction_eligible_date == '3 Years':
-                self.destruction_time = 94608000
-            elif destruction_eligible_date == '4 Years':
-                self.destruction_time = 126144000
-            elif destruction_eligible_date == '5 Years':
-                self.destruction_time = 157680000
-            elif destruction_eligible_date == '6 Years':
-                self.destruction_time = 189216000
-            elif destruction_eligible_date == '7 Years':
-                self.destruction_time = 220752000
-            elif destruction_eligible_date == '8 Years':
-                self.destruction_time = 252288000
-            else:
-                self.destruction_time = 31536000
+            if self.related_package.destruction_eligible_time:
+                self.destruction_time = self.DESTRUCTION_TIME_MAPPING.get(self.related_package.destruction_eligible_time, 31536000)
 
         super().save(*args, **kwargs)
 
@@ -56,3 +44,9 @@ class DestructionEligible(models.Model):
 
     class Meta:
         ordering = ['-created_at']
+
+@receiver(post_save, sender=Package)
+def update_destruction_eligibility(sender, instance, **kwargs):
+    # When a Package is saved, update the associated DestructionEligible instance
+    destruction_eligibility, created = DestructionEligible.objects.get_or_create(related_package=instance)
+    destruction_eligibility.save()
