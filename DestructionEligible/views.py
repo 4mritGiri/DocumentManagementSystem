dc697a2ab4743e98eafa22c40ec58d457b01bde6
4mritGiri <amritgiri02595@gmail.com>
2024-01-27 22:02:15 +0545

    Fix destruction eligible view and update QR code link

--- DestructionEligible/views.py
+++ DestructionEligible/views.py
@@ -10,4 +10,6 @@ from .models import DestructionEligible
 @login_required(login_url='login')
 def listDestructionEligible(request):
     destruction_eligibles = DestructionEligible.objects.all()
+
+    print(destruction_eligibles)
     return render(request, 'DestructionEligible/list_destruction_eligible.html', {'destruction_eligibles': destruction_eligibles})
