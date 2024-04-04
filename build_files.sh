b9b6c7400f513136b445484cb24217d1395faa1a
4mritGiri <amritgiri02595@gmail.com>
2024-04-04 06:46:56 +0545

    Update database settings and build files for vercel deployment

--- build_files.sh
+++ build_files.sh
@@ -1,2 +1,17 @@
+# Build files.sh for vercel deployment
+echo "Building files..."
+
+echo "Installing requirements..."
 pip install -r requirements.txt
-python3.11 manage.py collectstatic --noinput
+
+echo "Make migrations..."
+python3.11 manage.py makemigrations
+
+echo "Migrate..."
+python3.11 manage.py migrate
+
+echo "Collecting static files..."
+python3.11 manage.py collectstatic --noinput --clear
+
+echo "Files built successfully!"
+
