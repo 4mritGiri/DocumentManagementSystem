163832d4067fbe6585c4b9f7cfb5ff72cee1cc6a
4mritGiri <amritgiri02595@gmail.com>
2024-04-05 10:32:10 +0545

    Update settings.py and models.py, add print statement in manage.py

--- manage.py
+++ manage.py
@@ -6,6 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
+    print("Running main function...")
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'DocumentManagementSystem.settings')
     try:
         from django.core.management import execute_from_command_line
