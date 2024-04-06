96c613a21c84fb5244859d63f93f125c42c80b9d
@mr!t-G!r! <85377404+4mritGiri@users.noreply.github.com>
2024-04-06 10:01:46 +0545

    Update build_files.sh

--- build_files.sh
+++ build_files.sh
@@ -3,7 +3,7 @@
 echo "Building files..."
 
 echo "Installing requirements..."
-pip install -r requirements.txt
+pip3 install -r requirements.txt
 
 echo "Make migrations..."
 python3 manage.py makemigrations
