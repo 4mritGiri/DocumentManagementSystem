9e24ca3a0e264646f5483bc417ae302cd023751c
4mritGiri <amritgiri02595@gmail.com>
2024-04-08 01:10:25 +0545

    Commented out unnecessary migration commands

--- build_files.sh
+++ build_files.sh
@@ -12,10 +12,10 @@ echo "Installing requirements..."
 python3 -m pip install -r requirements.txt
 
 echo "Make migrations..."
-python3 manage.py makemigrations --noinput
+# python3 manage.py makemigrations --noinput
 
 echo "Migrate..."
-python3 manage.py migrate --noinput
+# python3 manage.py migrate --noinput
 
 echo "Collecting static files..."
 python3 manage.py collectstatic --noinput --clear
